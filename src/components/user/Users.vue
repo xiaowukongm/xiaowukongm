ed5a021fc8e2b90cb12a772b2f10966e57829043
xiaowukong <2363497179@qq.com>
2020-04-01 00:06:54 +0800

    渲染用户列表区域

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -18,16 +18,67 @@
           <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
+      <el-table border :data="userList" stripe style="width: 100%">
+        <el-table-column type="index" label="#">
+        </el-table-column>
+        <el-table-column prop="username" label="姓名">
+        </el-table-column>
+        <el-table-column prop="mobile" label="电话">
+        </el-table-column>
+        <el-table-column prop="email" label="邮箱">
+        </el-table-column>
+        <el-table-column prop="role_name" label="角色">
+        </el-table-column>
+        <el-table-column label="状态">
+          <template v-slot="scope">
+            <el-switch v-model="scope.row.mg_state"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template v-slot="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="warning" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
 
 <script>
     export default {
-        name: "Users"
+      name: "Users",
+      data(){
+          return{
+            // 用户数据请求参数
+            queryParams:{
+              query:',',
+              pagenum:1,
+              pagesize:3,
+            },
+            //用户数据列表
+            userList:[]
+          }
+      },
+      created() {
+        this.getUserList()
+      },
+      methods:{
+        getUserList(){
+          this.$http.get("users",{params:this.queryParams})
+          .then(response=>{
+            this.userList = response.data.users
+            // console.log(response.data.users)
+          })
+        }
+      }
     }
 </script>
 
 <style scoped>
-
+.el-table{
+  margin-top: 20px;
+  font-size: 13px;
+}
 </style>
